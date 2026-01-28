c93c77eab49be7cb3256f7a245c5f33b1610f33f
Dominika Szczotkowska <dszczotkowska@craftware.biz>
2026-01-28 22:38:45 +0100

    test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,29 +1,25 @@
 import { test, expect } from '../fixtures/baseTest'
 
+// TEST 1
 test('Verify articles section on landing page', async ({ landingPage }) => {
   
-  // Krok 1: Akcja
   await test.step('Navigate to Articles section', async () => {
     await landingPage.clickArticles();
   });
 
-  // Krok 2: Weryfikacja (zawsze warto dodać krok na sprawdzenie)
   await test.step('Verify that Articles header is displayed', async () => {
-    // Zakładamy, że masz taki lokator w landingPage
     await expect(landingPage.articlesHeader).toBeVisible();
   });
+}); // <--- Tutaj zamykamy pierwszy test
 
-  test('Verify 2', async ({ landingPage }) => {
+// TEST 2
+test('Verify 2', async ({ landingPage }) => {
   
-  // Krok 1: Akcja
   await test.step('ffff', async () => {
     await landingPage.clickArticles();
   });
 
-  // Krok 2: Weryfikacja (zawsze warto dodać krok na sprawdzenie)
   await test.step('kkkk', async () => {
-    // Zakładamy, że masz taki lokator w landingPage
     await expect(landingPage.articlesHeader).toBeVisible();
   });
-
-});
+}); // <--- Tutaj zamykamy drugi test
